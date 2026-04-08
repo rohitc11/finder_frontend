e1b0a65459c4c4f694a18be9c95ed9fd4d808392
Shrenik Thakkar <thakkar.shrenik@gmail.com>
2026-04-08 12:44:49 +0530

    Added flag changes

--- lib/config/feature_flags.dart
+++ lib/config/feature_flags.dart
@@ -10,7 +10,7 @@ class FeatureFlags {
   /// Important:
   /// - when false, only reward UI is hidden
   /// - contribution history and counters should still remain visible
-  static const bool isRewardsEnabled = true;
+  static const bool isRewardsEnabled = false;
 
   /// Quick actions launch toggles.
   ///
